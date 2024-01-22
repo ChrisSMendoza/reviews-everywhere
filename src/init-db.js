6e5d819e586c351721db0cd88a91c9cf4cf4a2fc
Chris Mendoza <chris_codes@aol.com>
2024-01-21 23:10:04 -0800

    add exploration DB files to init and find a review

---
+++ src/init-db.js
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const review = await prisma.review.create({
+        data: {
+            text: "This is the first persisted review"
+        }
+    })
+    console.log("New Review:");
+    console.log(review);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
