6663596f037964ca285945ead8eaea2613636243
Chris Mendoza <chris_codes@aol.com>
2024-01-22 00:19:21 -0800

    add example form data API handler, add basic form to show data is parsed

--- src/api.js
+++ src/api.js
@@ -1,13 +1,26 @@
 import express from 'express';
+// Side-effects allows form data to be parsed, no need for exported object
+import _ from 'body-parser';
 
 const app = express()
 const port = 3000
 
 app.use(express.static('static'));
 
+// Use middleware to parse URL-encoded form data
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/submit-form', (req, res) => {
+    const formData = req.body;
+
+    console.log(formData);
+
+    res.send('Form submitted successfully');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
