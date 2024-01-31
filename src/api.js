02e76c338b9fa65124a888198476105f4dea3d1c
Chris Mendoza <chris_codes@aol.com>
2024-01-31 02:00:44 -0800

    Refactor server (#12)
    * rename app Express object to api since will only house REST stuffs
    
    * remove no-dependency server since we use the vite integrated one
    
    * remove example vite express server since not used
    
    * remove ViteExpress setup and export Express app as default
    
    * add server file to glue API to vite setup
    
    * remove preview page from static since not used
    
    * update dev command to start the server with node
    
    * remove index within src since not used

--- src/api.js
+++ src/api.js
@@ -1,28 +1,26 @@
 import express from "express";
 // Side-effects allows form data to be parsed, no need for exported object
 import _ from "body-parser";
-import ViteExpress from "vite-express";
 
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const app = express();
-const PORT = 3000;
+const api = express();
 
-app.use(express.static("static"));
+api.use(express.static("static"));
 
 // Use middleware to parse URL-encoded form data
-app.use(express.urlencoded({ extended: true }));
+api.use(express.urlencoded({ extended: true }));
 
-app.get("/reviews", async (req, res) => {
+api.get("/reviews", async (req, res) => {
   // All reviews when no parameters are passed into `findMany`
   const reviews = await prisma.review.findMany();
 
   res.send(reviews);
 });
 
-app.post("/review", async (req, res) => {
+api.post("/review", async (req, res) => {
   // TODO: Add typing with JSDoc
   const reviewFromClient = req.body;
 
@@ -35,6 +33,4 @@ app.post("/review", async (req, res) => {
   res.send(`Review created`);
 });
 
-ViteExpress.listen(app, PORT, () =>
-  console.log(`Server is listening on ${PORT}`),
-);
+export default api;
--- src/server.js
+++ src/server.js
@@ -1,54 +1,9 @@
-import * as fs from "node:fs";
-import * as http from "node:http";
-import * as path from "node:path";
+import ViteExpress from "vite-express";
 
-const PORT = 8000;
+import api from "./api.js";
 
-const MIME_TYPES = {
-  default: "application/octet-stream",
-  html: "text/html; charset=UTF-8",
-  js: "application/javascript",
-  mjs: "application/javascript", // JS Modules
-  css: "text/css",
-  png: "image/png",
-  jpg: "image/jpg",
-  gif: "image/gif",
-  ico: "image/x-icon",
-  svg: "image/svg+xml",
-};
+const PORT = 3000;
 
-const STATIC_PATH = path.join(process.cwd(), "./static");
-
-const toBool = [() => true, () => false];
-
-const prepareFile = async (url) => {
-  const paths = [STATIC_PATH, url];
-  if (url.endsWith("/")) paths.push("index.html");
-  const filePath = path.join(...paths);
-  const pathTraversal = !filePath.startsWith(STATIC_PATH);
-  const exists = await fs.promises.access(filePath).then(...toBool);
-  const found = !pathTraversal && exists;
-  const streamPath = found ? filePath : STATIC_PATH + "/404.html";
-  const ext = path.extname(streamPath).substring(1).toLowerCase();
-  const stream = fs.createReadStream(streamPath);
-  return { found, ext, stream };
-};
-
-http
-  .createServer(async (req, res) => {
-    // TODO: Set this for only the development route, and only during development
-    // Enable CORS for all routes
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-    const file = await prepareFile(req.url);
-    const statusCode = file.found ? 200 : 404;
-    const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
-    res.writeHead(statusCode, { "Content-Type": mimeType });
-    file.stream.pipe(res);
-    console.log(`${req.method} ${req.url} ${statusCode}`);
-  })
-  .listen(PORT);
-
-console.log(`Server running at http://127.0.0.1:${PORT}/`);
+ViteExpress.listen(api, PORT, () =>
+  console.log(`Server is listening on ${PORT}`),
+);
