294a019a6b7075d70ed6372f333b3f5c6c144cdc
Chris Mendoza <chris_codes@aol.com>
2024-09-25 20:56:33 -0700

    Share base url (#17)
    * comment on setting DOM onclick elsewhere
    
    * add API client module to export base URL based on Vite env var
    
    * import and use base URL from api client so single source of truth
    
    * import and use base URL from api client in core module
    
    * note permissions in extension manifest is not in sync with env vars
    
    * update our base URL to current ngork URL

---
+++ src/api-client.js
@@ -0,0 +1,3 @@
+const LOCAL_BASE_URL = "https://localhost:3000";
+
+export const BASE_URL = import.meta.env.VITE_OUR_BASE_URL ?? LOCAL_BASE_URL;
--- src/demo-reviews.js
+++ src/demo-reviews.js
@@ -1,5 +1,6 @@
 import van from "vanjs-core";
 
+import { BASE_URL } from "./api-client";
 import { OverlayReview } from "./review";
 import { onDocumentClick } from "./reviews-everywhere";
 
@@ -8,9 +9,6 @@ import { onDocumentClick } from "./reviews-everywhere";
 // const BASE_URL = "http://localhost:3000";
 // if extension, then use hosted version
 // should be set by env / build process
-
-const BASE_URL =
-  "https://6e9d-2600-1700-5b24-a090-dcd4-affe-ad06-55fd.ngrok-free.app";
 
 async function loadReviews() {
   const reviewsResponse = await fetch(`${BASE_URL}/reviews`);
@@ -38,4 +36,6 @@ async function loadReviews() {
 loadReviews().then(console.log).catch(console.error);
 
 console.log("Context menu will appear on click - Reviews Everywhere");
+
+// TODO: Move this into another file, process, build?? IDK??
 document.addEventListener("click", onDocumentClick);
