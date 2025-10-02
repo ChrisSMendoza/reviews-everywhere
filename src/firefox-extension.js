d46eee90704f76f344771638fb39055f4584c2d6
Chris Mendoza <chris@getformalize.com>
2025-10-02 00:18:08 -0700

    add back doc click for create review form

--- src/firefox-extension.js
+++ src/firefox-extension.js
@@ -1,6 +1,13 @@
+import { onDocumentClick } from "./reviews-everywhere.js";
 import { renderReviews } from "./review";
 
 
+// TODO: Fix creating reviews, needs to happen in the background script (like the fetch below)
+// Show 'Create Review' form on click
+document.addEventListener("click", onDocumentClick);
+
+
+// Ask background script to fetch Reviews / Messages
 const sending = browser.runtime.sendMessage({
     action: "fetchMessages",
 });
