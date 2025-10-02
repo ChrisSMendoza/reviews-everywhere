2476b8a881d3e89218c195f577566d03da4f7a69
Chris Mendoza <chris@getformalize.com>
2025-10-01 22:09:36 -0700

    refactor fetch reviews to use async and single then when called

--- src/extension-background.js
+++ src/extension-background.js
@@ -15,31 +15,36 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log("Background script: sender:", sender)
 
     if (message.action === FETCH_MESSAGES_ACTION) {
-        console.log(`Background received ${FETCH_MESSAGES_ACTION} action`);
-        const BASE_URL = "https://b73efe21c6c4.ngrok-free.app";
-        const getReviewsRequest = new URL(`${BASE_URL}/reviews`);
+        console.log("Background: Fetching reviews");
 
-        getReviewsRequest.searchParams.append("windowHref", sender.url);
+        // TODO: Add catch here, can we just throw in fetchReviews?
+        fetchReviews({ windowHref: sender.url })
+            .then(reviews => sendResponse({ reviews }))
 
-        console.log("Fetching reviews", { getReviewsRequest });
-
-        // Using `return` doesn't seem to get send the response object (maybe because it can't be serialized, but no error shown)
-        fetch(getReviewsRequest)
-            .then(res => {
-
-                if(res.ok) {
-                    console.log("Background: reviews fetched successfully");
-
-                    res.json().then(reviews => {
-                        console.log("Background: reviews as JSON", reviews);
-
-                        sendResponse({ reviews, windowHref: sender.url });
-                    })
-                }
-            });
+        // NOTE: Using `return` with response Promise doesn't seem to send it to client listener (maybe because it can't be serialized, but no error shown)
     }
 
     // This is needed to run an asynchronous task with `sendResponse` on both Firefox and Chrome
     // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/onMessage#sending_an_asynchronous_response_using_sendresponse
     return true;
 });
+// TODO: Revisit pattern, don't like that error isn't handled and we're giving back null. Throw instead?
+async function fetchReviews(options) {
+    const BASE_URL = "https://b73efe21c6c4.ngrok-free.app";
+    const getReviewsRequest = new URL(`${BASE_URL}/reviews`);
+
+    getReviewsRequest.searchParams.append("windowHref", options.windowHref);
+
+    const reviewsResponse = await fetch(getReviewsRequest);
+
+    if (reviewsResponse.ok) {
+        console.log("Background: reviews fetched successfully");
+
+        const reviews = await reviewsResponse.json();
+
+        return reviews;
+    }
+
+    console.warn("Bad status when fetching reviews", reviewsResponse);
+    return null;
+}
