a141d2dfc3a859b4352c4ff61533dba6c50241f9
Chris Mendoza <chris@getformalize.com>
2025-10-01 18:27:20 -0700

    only run onMessage when active tabs sent the message

--- src/extension-background.js
+++ src/extension-background.js
@@ -3,10 +3,18 @@ console.log("`extension-background.js` has ran!");
 
 
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log("Background script: onMessage");
 
-  if (message.action === "fetchData") {
-    console.log("Background received fetchData message");
+    // Ignore messages from inactive tabs
+    if(!sender.tab.active) {
+        return;
+    }
 
-    return true; // Indicates that sendResponse will be called asynchronously
-  }
+    console.log("Background script: sender:", sender)
+
+    if (message.action === "fetchData") {
+        console.log("Background received fetchData message");
+
+        return true; // Indicates that sendResponse will be called asynchronously
+    }
 });
